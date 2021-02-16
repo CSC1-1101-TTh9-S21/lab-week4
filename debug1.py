f5963e40c9b6f30f3f3e206d47ff6d435bde3eac
Emily Prud'hommeaux <prudhome@bc.edu>
2021-02-15 20:59:28 -0500

    Add files via upload

---
+++ debug1.py
@@ -0,0 +1,6 @@
+def main():
+    name = input("What is your name? ")
+    print("Hello,", name)
+    if "A" in name:
+        print("Your name begins with A")
+
---
+++ debug3.py
@@ -0,0 +1,11 @@
+n1 = int(input("First number: ")) 
+n2 = int(input("First number: ")) 
+n3 = int(input("First number: ")) 
+getmean(n1, n2, n3)
+
+
+def getmean(a,b,c):
+    mymean = (a+b+c)/3
+    print(mymean)
+
+
